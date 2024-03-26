f7e26695d68d52ee4b26b8704dd42622f6d22d33
mike-menyala <159994059+mike-menyala@users.noreply.github.com>
2024-03-26 20:51:28 +0800

    commit message for multiple files

--- packages/hardhat/contracts/DecentralizedExchange.sol
+++ packages/hardhat/contracts/DecentralizedExchange.sol
@@ -9,7 +9,7 @@ import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 /**
  * @title Decentralized Exchange
- * @dev This contract implements a simple decentralized exchange where users can trade ERC20 tokens.
+ * @dev This contract implements a simple decentralized exchange where users can trade ERC20 tokens with limit orders based on specific price conditions.
  */
 contract DecentralizedExchange {
     using SafeERC20 for ERC20;
@@ -19,36 +19,39 @@ contract DecentralizedExchange {
         address trader;
         address tokenAddress;
         uint256 amount;
+        uint256 price; // New field to store the price of the order
     }
 
     mapping(bytes32 => Order) public orderBook;
 
     /**
-     * @dev Function to submit a buy limit order.
+     * @dev Function to submit a buy limit order with a specific price condition.
      * @param tokenAddress The address of the token to buy.
      * @param amount The amount of tokens to buy.
+     * @param price The price at which the user wants to buy the tokens.
      */
-    function submitBuyOrder(address tokenAddress, uint256 amount) external {
+    function submitBuyOrder(address tokenAddress, uint256 amount, uint256 price) external {
         ERC20 token = ERC20(tokenAddress);
         token.safeTransferFrom(msg.sender, address(this), amount);
-        bytes32 orderId = keccak256(abi.encode(msg.sender, tokenAddress, amount));
-        orderBook[orderId] = Order(msg.sender, tokenAddress, amount);
+        bytes32 orderId = keccak256(abi.encode(msg.sender, tokenAddress, amount, price));
+        orderBook[orderId] = Order(msg.sender, tokenAddress, amount, price);
     }
 
     /**
-     * @dev Function to submit a sell limit order.
+     * @dev Function to submit a sell limit order with a specific price condition.
      * @param tokenAddress The address of the token to sell.
      * @param amount The amount of tokens to sell.
+     * @param price The price at which the user wants to sell the tokens.
      */
-    function submitSellOrder(address tokenAddress, uint256 amount) external {
+    function submitSellOrder(address tokenAddress, uint256 amount, uint256 price) external {
         ERC20 token = ERC20(tokenAddress);
         token.safeTransferFrom(msg.sender, address(this), amount);
-        bytes32 orderId = keccak256(abi.encode(msg.sender, tokenAddress, amount));
-        orderBook[orderId] = Order(msg.sender, tokenAddress, amount);
+        bytes32 orderId = keccak256(abi.encode(msg.sender, tokenAddress, amount, price));
+        orderBook[orderId] = Order(msg.sender, tokenAddress, amount, price);
     }
 
     /**
-     * @dev Function to execute a trade between two orders.
+     * @dev Function to execute a trade between two orders based on price conditions.
      * @param buyOrderId The ID of the buy order.
      * @param sellOrderId The ID of the sell order.
      */
@@ -57,6 +60,7 @@ contract DecentralizedExchange {
         Order memory sellOrder = orderBook[sellOrderId];
         require(buyOrder.trader != address(0), "Buy order does not exist");
         require(sellOrder.trader != address(0), "Sell order does not exist");
+        require(buyOrder.price >= sellOrder.price, "Buy price is lower than sell price");
 
         ERC20 buyToken = ERC20(buyOrder.tokenAddress);
         ERC20 sellToken = ERC20(sellOrder.tokenAddress);
